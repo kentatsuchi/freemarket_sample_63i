8c0adaa1cfe0d758869d09a7637fc236a28d3d1b
kentatsuchi <ken.sb.bb@gmail.com>
2019-12-14 18:44:22 +0900

    Revert "バリデーションの表示"
    This reverts commit 598f505067761d7fd7fcccfd90d612be9d00484e.

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,10 +1,3 @@
 class Card < ApplicationRecord
-
   belongs_to :user
-
-  validates :nickname,                presence: true, length: {maximum: 20}, uniqueness: true
-  validates :email,                   presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password,      presence: true, length: {minimum: 7, maximum: 128}
-
 end
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,5 @@ class User < ApplicationRecord
   validates :email,                   presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password,      presence: true, length: {minimum: 7, maximum: 128}
 
+  
 end
