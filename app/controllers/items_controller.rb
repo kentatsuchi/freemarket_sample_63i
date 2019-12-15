9d6bbb5a5d355dde00cc7250bcdc953813cee463
kentatsuchi <ken.sb.bb@gmail.com>
2019-12-15 15:11:50 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 
   before_action :move_to_index, except: :index
   
-  before_action :set_item,only: [:show,:edit,:update,:destroy]
+  before_action :set_item,only: [:show,:edit,:destroy]
 
 
   def index
